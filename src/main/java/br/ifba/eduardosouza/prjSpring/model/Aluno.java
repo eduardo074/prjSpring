9b2bde33395154694745567650fde42e688a2354
Eduardo <103686965+eduardo074@users.noreply.github.com>
2023-04-23 17:51:45 -0300

    Adicionado antoções de validação

--- src/main/java/br/ifba/eduardosouza/prjSpring/model/Aluno.java
+++ src/main/java/br/ifba/eduardosouza/prjSpring/model/Aluno.java
@@ -6,19 +6,34 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
 // data vai gerar todos os get e set automático
 @Data
 @Entity
 @Table(name="alunos")
 public class Aluno extends AbstractEntity{
+
+    @Column(nullable = false, unique = true)
+    @Size(min = 5, max = 20)
     private String user_name;
+    @Size(min = 5, max = 20)
     private String first_name;
+    @Size(min = 5, max = 20)
     private String last_name;
+    @Email
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String phone;
+
+    @Column(nullable = false)
+    @Size(min = 6, max = 255)
     private String senha;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String matricula;
 
     public Aluno() {
--- src/main/java/br/ifba/eduardosouza/prjSpring/model/User.java
+++ src/main/java/br/ifba/eduardosouza/prjSpring/model/User.java
@@ -6,17 +6,22 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
 @Entity
 @Data
 @Table(name="Users")
 public class User extends AbstractEntity{
-
-
     @Column(nullable = false)
     private String full_name;
     @Column(unique = true, nullable = false)
+    @Email
     private String email;
+    @Size(min = 5, max = 255)
     private String password;
+    @Column(nullable = false)
+    @Size(min = 5, max = 255)
     private String address;
     private boolean active;
 }
