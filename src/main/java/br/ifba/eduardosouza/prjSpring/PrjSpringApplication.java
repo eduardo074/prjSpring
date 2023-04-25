fb70acd9081c9c3f0f06cb09161ec75bc5160206
Eduardo <103686965+eduardo074@users.noreply.github.com>
2023-04-25 10:07:22 -0300

    Removendo as views do swing

--- src/main/java/br/ifba/eduardosouza/prjSpring/PrjSpringApplication.java
+++ src/main/java/br/ifba/eduardosouza/prjSpring/PrjSpringApplication.java
@@ -1,6 +1,4 @@
 package br.ifba.eduardosouza.prjSpring;
-
-import br.ifba.eduardosouza.prjSpring.views.Dashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,15 +8,6 @@ import java.awt.*;
 public class PrjSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PrjSpringApplication.class, args);
-
-
-		if (!GraphicsEnvironment.isHeadless()) {
-			java.awt.EventQueue.invokeLater(new Runnable() {
-				public void run() {
-					new Dashboard().setVisible(true);
-				}
-			});
-		}
 	}
 
 }
